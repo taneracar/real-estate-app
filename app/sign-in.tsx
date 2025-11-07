20e0c9e4a7fcb88bdcfaecc47c3db2e18e6dd544
taner.acar <taneracar1@icloud.com>
2025-11-07 13:41:57 +0300

    implement google login

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,31 +1,80 @@
-import icons from '@/constants/icons'
-import images from '@/constants/images'
-import React from 'react'
-import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from "react";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const handleLogin = () => {}
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
 
-const SingIn = () => {
+const Auth = () => {
+  const { refetch, loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/" />;
+
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to login");
+    }
+  };
+
   return (
-    <SafeAreaView className='bg-white h-full'>
-     <ScrollView contentContainerClassName='h-full'>
-    <Image className='w-full h-4/6' resizeMode='contain' source={images.onboarding} />
-    <View className='px-10'> 
-    <Text className='text-center font-rubik text-base uppercase text-black-200'>Welcome to ReState</Text>
-    <Text className='text-3xl font-rubik-bold text-black-300 mt-2 text-center capitalize'> Let&apos;s get you Closer to {'\n'} <Text className='text-primary-300'>your ideal home</Text></Text>
-    <Text className='text-center font-rubik text-lg text-black-200 mt-12'>Login with Google</Text>
-    <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
-  
-    <View className='flex flex-row items-center justify-center'>  <Image className='w-5 h-5' resizeMode='contain' source={icons.google} />
-    <Text className='text-lg font-rubik-medium text-black-300 ml-4'>Continue with Google</Text>
-    </View>
-    </TouchableOpacity>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
 
-    </View>
-     </ScrollView>
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To Real Scout
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let&apos;s Get You Closer To {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Real Scout with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SingIn
+export default Auth;
